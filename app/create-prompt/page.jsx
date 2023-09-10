5683d307ab61629a6a30a458c9332c1b4081bdb0
Simondelali <atiegardelali@gmail.com>
2023-09-10 21:01:26 +0000

    create post

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+"use client"
+
+import { useState} from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from 'components/Form';
 
 const createPrompt = () => {
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+
+    const createPrompt = async (e) => {
+    }
   return (
-    <div>createPrompt</div>
+    <Form
+    type='Create'
+    post={post}
+    setPost={setPost}
+    submitting={submitting}
+    handleSubmit={createPrompt}
+    />
   )
 }
 
-export default createPrompt
+export default createPrompt 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -31,6 +31,7 @@ const Nav = () => {
           height={30}
           className="object-contain"          
           />
+          <p className='logo_text'>Promptopia</p>
            
           
         </Link>
